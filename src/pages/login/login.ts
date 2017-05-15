cdf2ef683e82b2fcfd4838eb26cdc2bcd62f6390
atmosmaciel <atmos.mps@gmail.com>
2017-05-15 14:17:00 -0300

    configurando token para acessar API no login

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,24 +1,38 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
-/**
- * Generated class for the LoginPage page.
- *
- * See http://ionicframework.com/docs/components/#navigation for more info
- * on Ionic pages and navigation.
- */
-@IonicPage()
+import { NavController, NavParams } from 'ionic-angular';
+import {Http} from "@angular/http";
+import 'rxjs/add/operator/toPromise';
+import {ProductListPage} from "../product-list/product-list";
+/*
+  Generated class for the Login page.
+  See http://ionicframework.com/docs/v2/components/#navigation for more info on
+  Ionic pages and navigation.
+*/
 @Component({
   selector: 'page-login',
-  templateUrl: 'login.html',
+  templateUrl: 'login.html'
 })
 export class LoginPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
+  public user = {
+    email: '',
+    password: ''
+  };
+
+  constructor(
+      public navCtrl: NavController, public navParams: NavParams,
+      public http: Http
+  ) {}
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad LoginPage');
   }
 
+  login() {
+      this.http.post('http://localhost:8000/api/login',this.user)
+          .toPromise().then((response) => {
+        window.localStorage['token'] = response.json().token;
+        this.navCtrl.setRoot(ProductListPage)
+      });
+  }
 }
